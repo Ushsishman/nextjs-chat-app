c2e85647973a4ced99891a623a8c49ba9fbfa827
Musa Akdeniz <musa191690@gmail.com>
2024-11-16 14:50:01 +0300

    Add login and register forms and configure Firebase

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import RegisterForm from "@/components/form/RegisterForm";
+
+const page = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/form/LoginForm.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const LoginForm = () => {
+  const formSchema = z.object({
+    email: z
+      .string()
+      .min(1, { message: "This field has to be filled." })
+      .max(20)
+      .email("This is not a valid email."),
+    password: z
+      .string()
+      .min(5, { message: "Password should be longer." })
+      .max(20),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-60 p-3 border-2 border-black rounded space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <FormControl>
+                <Input id="email" type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <FormControl>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant="outline" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default LoginForm;
