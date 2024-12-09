fc9de5ff7872bfc479c32cf0b88f6f83dddb02c7
Musa Akdeniz <musa191690@gmail.com>
2024-12-10 02:17:26 +0300

    Update sidebar and chatcontainer ui for rooms functionality.

--- src/components/home/ChatContainer.tsx
+++ src/components/home/ChatContainer.tsx
@@ -6,6 +6,16 @@ import ChatMain from "./ChatMain";
 import { useUsers } from "@/contexts/usersContext";
 import bgChat from "../../../public/chat.png";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "../ui/input";
 
 const ChatContainer = () => {
   const { clickedUser } = useUsers();
@@ -19,7 +29,7 @@ const ChatContainer = () => {
           <ChatInput />
         </div>
       ) : (
-        <div className="h-screen flex flex-col items-center justify-center space-y-3">
+        <div className="h-screen flex flex-col items-center justify-center space-y-3 text-lg">
           <div className="absolute -z-10">
             <Image
               src={bgChat}
@@ -28,9 +38,22 @@ const ChatContainer = () => {
             />
           </div>
           <h1 className="text-4xl font-bold">Welcome to ChatApp!</h1>
-          <p className="text-lg">
-            Select a user to start chatting or search for someone new.
-          </p>
+          <p>Select a user to start chatting or search for someone new.</p>
+          <p>or</p>
+          <Dialog>
+            <DialogTrigger className="text-lg bg-[#D99559] hover:bg-[#F2B441] text-[#F2E2CE] py-2 px-4 rounded-full">
+              Create Room
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Room Name</DialogTitle>
+                <DialogDescription className="flex flex-row space-x-2">
+                  <Input placeholder="Room name..." />
+                  <Button>Create</Button>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       )}
     </div>
--- src/components/sidebar/SidebarMain.tsx
+++ src/components/sidebar/SidebarMain.tsx
@@ -7,8 +7,14 @@ import User from "../User/User";
 import { Key } from "react";
 import { UserData } from "@/interfaces/user";
 import { useAuth } from "@/contexts/authContext";
+import { useState } from "react";
+import clsx from "clsx";
+import { Button } from "../ui/button";
 
 const SidebarMain = ({ inputText }: { inputText: string }) => {
+  const [currentSidebarTab, setCurrentSidebarTab] = useState<"users" | "rooms">(
+    "users",
+  );
   const { allUsers, setClickedUser } = useUsers();
   const { currentUser } = useAuth();
 
@@ -21,15 +27,38 @@ const SidebarMain = ({ inputText }: { inputText: string }) => {
   return (
     <ScrollArea className="flex-1 w-full rounded-md border text-[#F2E2CE]">
       <div className="p-4">
-        <h4 className="mb-4 text-lg font-bold">Users</h4>
-        {searchBarArr.map((user: UserData, index: Key | null | undefined) => (
-          <div key={index}>
-            <div onClick={() => setClickedUser(user)}>
-              <User user={user} />
-            </div>
-            <Separator className="my-2" />
+        <div className="flex flex-row justify-around items-center border-b-2 mb-2">
+          <Button
+            onClick={() => setCurrentSidebarTab("users")}
+            className={clsx("mb-4 py-2 px-4 rounded-md text-lg font-bold", {
+              underline: currentSidebarTab === "users",
+            })}>
+            Users
+          </Button>
+          <Button
+            onClick={() => setCurrentSidebarTab("rooms")}
+            className={clsx("mb-4 py-2 px-4 rounded-md text-lg font-bold", {
+              underline: currentSidebarTab === "rooms",
+            })}>
+            Rooms
+          </Button>
+        </div>
+        {currentSidebarTab === "users" ? (
+          <div>
+            {searchBarArr.map(
+              (user: UserData, index: Key | null | undefined) => (
+                <div key={index}>
+                  <div onClick={() => setClickedUser(user)}>
+                    <User user={user} />
+                  </div>
+                  <Separator className="my-2" />
+                </div>
+              ),
+            )}
           </div>
-        ))}
+        ) : (
+          <p>rooms</p>
+        )}
       </div>
     </ScrollArea>
   );
